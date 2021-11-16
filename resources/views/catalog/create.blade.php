ff83fba936249b973f379b2a4d9dd6a8bb8f77f0
Alejandra964 <ale254126@gmail.com>
2021-11-16 15:09:00 -0400

    Se modificaron las validaciones

--- resources/views/catalog/create.blade.php
+++ resources/views/catalog/create.blade.php
@@ -9,23 +9,33 @@
             <div class="form-floating mb-3">
                 <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Titulo" value="{{old('titulo')}}">
                 <label for="titulo">Titulo</label>
+                @error('titulo')
+                <div class="text-danger">{{$message}}</div>
+                @enderror
               </div>
 
               <div class="form-floating mb-3">
                 <input type="text" class="form-control" id="director" name="director" placeholder="Director" value="{{old('director')}}">
                 <label for="director">Director</label>
+                @error('director')
+                <div class="text-danger">{{$message}}</div>
+                @enderror
               </div>
 
               <div class="mb-3">
                 <label for="poster" class="form-label">Poster</label>
                 <input class="form-control" type="file" id="poster" name="poster">
+                @error('poster')
+                <div class="text-danger">{{$message}}</div>
+                @enderror
               </div>
-
-
 
               <div class="form-floating mb-3">
                 <input type="number" class="form-control" id="year" name="year" placeholder="Year" value="{{old('year')}}">
                 <label for="year">Año</label>
+                @error('year')
+                <div class="text-danger">{{$message}}</div>
+                @enderror
               </div>
               <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" id="rentada" name="rentada">
@@ -37,18 +47,14 @@
               <div class="form-floating mb-3 ">
                 <textarea class="form-control" placeholder="Sinopsis" name="sinopsis" id="sinopsis" style="height: 100px" value="{{old('titulo')}}"></textarea>
                 <label for="sinopsis">Sinopsis</label>
+                @error('sinopsis')
+                <div class="text-danger">{{$message}}</div>
+                @enderror
               </div>
 
               <button class="btn btn-primary" type="submit">Guardar</button>
         </form>
-        @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
 
-                @endforeach
-            </ul>
-        @endif
     </div>
 </div>
 
